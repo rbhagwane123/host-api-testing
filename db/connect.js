7f6858f17c936fcef31e984e02bb5e1af5aacf66
Rupesh <84093647+rbhagwane123@users.noreply.github.com>
2024-07-01 15:05:03 +0530

    New data adding

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
-var MongoClient = require('mongodb').MongoClient;
-uri = "mongodb://0.0.0.0:27017/";
-const client = new MongoClient(uri);
-const connectDb = () => {
+var MongoClient = require("mongodb").MongoClient;
+// uri =
+//   "mongodb+srv://riokumar3757:124MivEO322U3igf@rupestestingapi.pt7y0o7.mongodb.net/rupestestingapi?retryWrites=true&w=majority&appName=RupesTestingApi";
+// const client = new MongoClient(uri);
+const connectDb = (uri) => {
   console.log("Inside connected database");
+  console.log(uri);
   return mongoose.connect(uri);
 };
 
 module.exports = connectDb;
+
+// riokumar3757
+// 124MivEO322U3igf
