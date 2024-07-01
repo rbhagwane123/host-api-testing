db3b36f0ba2efcb525fd8404eadf894f4f2df722
Rupesh <84093647+rbhagwane123@users.noreply.github.com>
2024-07-01 14:31:33 +0530

    second commit

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
+var MongoClient = require('mongodb').MongoClient;
 uri = "mongodb://0.0.0.0:27017/";
+const client = new MongoClient(uri);
 const connectDb = () => {
   console.log("Inside connected database");
   return mongoose.connect(uri);
